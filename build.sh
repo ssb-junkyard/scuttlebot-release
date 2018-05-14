d776d09ee0c6241f4eb90d4016b97b655ee47322
Dominic Tarr <dominic.tarr@gmail.com>
2018-05-14 19:51:16 +1200

    8.3.1

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
 rm npm-shrinkwrap.json
-npm install scuttlebot@$1 --package-lock
-npm shrinkwrap
-node update.js $1
-npm install #install dev deps
-git add .
-git commit -m $1
-npm test && {
-  npm publish
+npm install scuttlebot@$1 --package-lock && {
+  npm shrinkwrap
+  node update.js $1
+  npm install #install dev deps
+  git add .
+  git commit -m $1
+  npm test && {
+    npm publish
+  }
 }
 
