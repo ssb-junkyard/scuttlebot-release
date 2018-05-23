e1fb75368987b8995a40fee6333d77d4c22e2480
Dominic Tarr <dominic.tarr@gmail.com>
2018-05-23 22:46:13 +1200

    11.3.0-1

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 rm npm-shrinkwrap.json
 npm install scuttlebot@$1 --package-lock && {
   npm shrinkwrap
-  node update.js $1
+  node update.js $1 $2
   npm install #install dev deps
   git add .
-  git commit -m $1
+  git commit -m $1$2
+  hash=`shasum -a 256 < npm-shrinkwrap.json`
+  echo $1$2 ${hash:0:64} >> hashes.txt
   npm test && {
     npm publish
   }
--- update.js
+++ update.js
@@ -3,12 +3,12 @@ var _pkg = require('scuttlebot/package.json')
 var pkg = require('./package.json')
 
 var version = process.argv[2]
-
+var build = process.argv[3]
 pkg.devDependencies = _pkg.devDependencies
 pkg.dependencies = {
   scuttlebot: version
 }
-pkg.version = version
+pkg.version = version + build
 
 require('fs').writeFileSync(require('path').join(__dirname, 'package.json'), JSON.stringify(pkg, null, 2))
 
