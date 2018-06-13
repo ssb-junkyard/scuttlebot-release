37c51ceb088d4ea8c480307a1dad99c309f4564d
Dominic Tarr <dominic.tarr@gmail.com>
2018-06-13 20:57:27 +1200

    commit sha256 hashes of each npm-shrinkwrap.json

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@ npm install scuttlebot@$1 --package-lock && {
   npm shrinkwrap
   node update.js $1 $2
   npm install #install dev deps
-  git add .
-  git commit -m $1$2
   hash=`shasum -a 256 < npm-shrinkwrap.json`
   echo $1$2 ${hash:0:64} >> hashes.txt
+  git add .
+  git commit -m $1$2
   npm test && {
     npm publish
   }
 }
+
 
